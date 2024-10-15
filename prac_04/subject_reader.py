5ad5e08cc341f7dd9eb93a0e5b16b57607860934
aksasusanalias <aksa.susanalias@my.jcu.edu.au>
2024-10-15 14:33:27 +1000

    display subject details in formatted output

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,11 +9,13 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
+    display_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -21,9 +23,17 @@ def load_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+        data.append(parts)
 
+    input_file.close()
+    return data
+
+
+def display_subject_details(data):
+    for subject_data in data:
+        code = subject_data[0]
+        teacher = subject_data[1]
+        students = subject_data[2]
+        print(f"{code} is taught by {teacher} and has {students} students")
 
 main()
